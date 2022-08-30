9be293c61e87545555653f91bd5f6c3073dfa22b
Cole Kampa <ckampa13@gmail.com>
2022-08-30 12:32:08 -0500

    Added simple bash script to run all plotting scripts.

---
+++ EFTAnalysisFitting/scripts/drive_plots.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Run plotting scripts for all levels of the analysis, based on channel info in datacard_dict.
+
+source $CONDA_PREFIX/etc/profile.d/conda.sh
+conda activate HCOMB
+
+# run NLL plots
+python NLL_limits_plot_from_dir.py
+# run limit summary plots
+python NLL_limits_plot_from_dir.py
