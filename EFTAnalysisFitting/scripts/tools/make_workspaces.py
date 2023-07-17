0cbb3be928c2c3a5a30f67289e2c53f14d963052
Cole Kampa <ckampa13@gmail.com>
2023-07-17 17:22:56 -0500

    make_workspaces.py updated for multiple WCs. Fixed an error in systematics lines for 2L_SS.

--- EFTAnalysisFitting/scripts/tools/make_workspaces.py
+++ EFTAnalysisFitting/scripts/tools/make_workspaces.py
@@ -14,7 +14,7 @@ import sys
 fpath = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(fpath,'..'))
 from DATACARD_DICT import datacard_dict
-from CONFIG_VERSIONS import versions_dict
+from CONFIG_VERSIONS import versions_dict, WC_ALL
 from MISC_CONFIGS import template_filename, datacard_dir
 
 str_module = '-P HiggsAnalysis.AnalyticAnomalousCoupling.AnomalousCouplingEFTNegative:analiticAnomalousCouplingEFTNegative'
@@ -121,6 +121,9 @@ def make_workspace_channels(datacard_dict, WC, ScanType, verbose=1, StatOnly=Fal
     dcdir = os.path.join(datacard_dir, 'combined_datacards', 'channel')
     channels = datacard_dict.keys()
     for i, ch in enumerate(channels):
+        WCs = versions_dict[ch]['EFT_ops']
+        if not WC in WCs:
+            continue
         print(f'Channel: {ch}', end=': ')
         cmd_str = 'text2workspace.py '
         v = versions_dict[ch]['v']
@@ -187,60 +190,81 @@ def make_workspace_full_analysis(WC, ScanType, verbose=1, StatOnly=False):
 if __name__=='__main__':
     # parse commmand line arguments
     parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--Channel',
+                        help=f'Which channel? ["all" (default), "0Lepton_2FJ", "0Lepton_3FJ", "2Lepton_OS", "2Lepton_SS"]')
     parser.add_argument('-w', '--WC',
-                        help=f'Which Wilson Coefficient to study for 1D limits? ["cW" (default),]')
+                        help=f'Which Wilson Coefficient to study for 1D limits? ["all" (default), "cW", ...]')
     parser.add_argument('-s', '--ScanType',
                         help=f'What type of EFT scan was included in this file? ["_1D" (default),]')
     parser.add_argument('-V', '--Verbose',
                         help=f'Include "combine" output? 0 / 1 (default). "combine" output included if Verbose>0.')
     args = parser.parse_args()
+    # list of channels
+    if args.Channel is None:
+        args.Channel = 'all'
+    if args.Channel == 'all':
+        channels = datacard_dict.keys()
+    else:
+        channels = [args.Channel]
     if args.WC is None:
-        args.WC = 'cW'
+        args.WC = 'all'
+    if args.WC == 'all':
+        WCs_loop = WC_ALL
+    else:
+        WCs_loop = [args.WC]
     if args.ScanType is None:
         args.ScanType = '_1D'
     if args.Verbose is None:
         args.Verbose = 1
     else:
         args.Verbose = int(args.Verbose)
-    # TEST
-    print(args.WC, args.ScanType)
     #########################
-    # bins workspaces
-    print('Generating bin workspaces:')
-    print('=================================================')
-    for channel in datacard_dict.keys():
-        v = versions_dict[channel]['v']
-        VERSION = f'v{v}'
-        print(channel, VERSION)
+    # outer loop (over WC)
+    for WC in WCs_loop:
+        print(f'WC: '+WC)
+        #########################
+        # bins workspaces
+        print('Generating bin workspaces:')
+        print('=================================================')
+        for channel in channels:
+            WCs = versions_dict[channel]['EFT_ops']
+            if not WC in WCs:
+                continue
+            v = versions_dict[channel]['v']
+            VERSION = f'v{v}'
+            print(channel, VERSION)
+            for StatOnly in [False, True]:
+                print('Stat only? ', StatOnly)
+                make_workspace_bins(channel, VERSION, datacard_dict, WC=WC, ScanType=args.ScanType, verbose=args.Verbose, StatOnly=StatOnly)
+        print('=================================================\n')
+        #########################
+        # subchannel workspaces
+        print('Generating subchannel workspaces:')
+        print('=================================================')
+        for channel in channels:
+            WCs = versions_dict[channel]['EFT_ops']
+            if not WC in WCs:
+                continue
+            v = versions_dict[channel]['v']
+            VERSION = f'v{v}'
+            for StatOnly in [False, True]:
+                print('Stat only? ', StatOnly)
+                make_workspace_subchannels(channel, VERSION, datacard_dict, WC=WC, ScanType=args.ScanType, verbose=args.Verbose, StatOnly=StatOnly)
+        print('=================================================\n')
+        #########################
+        # channel workspaces
+        print('Generating channel workspaces:')
+        print('=================================================')
         for StatOnly in [False, True]:
             print('Stat only? ', StatOnly)
-            make_workspace_bins(channel, VERSION, datacard_dict, WC=args.WC, ScanType=args.ScanType, verbose=args.Verbose, StatOnly=StatOnly)
-    print('=================================================\n')
-    #########################
-    # subchannel workspaces
-    print('Generating subchannel workspaces:')
-    print('=================================================')
-    for channel in datacard_dict.keys():
-        v = versions_dict[channel]['v']
-        VERSION = f'v{v}'
+            make_workspace_channels(datacard_dict, WC=WC, ScanType=args.ScanType, verbose=args.Verbose, StatOnly=StatOnly)
+        print('=================================================\n')
+        #########################
+        # full analysis workspace
+        print('Generating full analysis workspace:')
+        print('=================================================')
         for StatOnly in [False, True]:
             print('Stat only? ', StatOnly)
-            make_workspace_subchannels(channel, VERSION, datacard_dict, WC=args.WC, ScanType=args.ScanType, verbose=args.Verbose, StatOnly=StatOnly)
-    print('=================================================\n')
-    #########################
-    # channel workspaces
-    print('Generating channel workspaces:')
-    print('=================================================')
-    for StatOnly in [False, True]:
-        print('Stat only? ', StatOnly)
-        make_workspace_channels(datacard_dict, WC=args.WC, ScanType=args.ScanType, verbose=args.Verbose, StatOnly=StatOnly)
-    print('=================================================\n')
-    #########################
-    # full analysis workspace
-    print('Generating full analysis workspace:')
-    print('=================================================')
-    for StatOnly in [False, True]:
-        print('Stat only? ', StatOnly)
-        make_workspace_full_analysis(WC=args.WC, ScanType=args.ScanType, verbose=args.Verbose, StatOnly=StatOnly)
-    print('=================================================\n')
-    #########################
+            make_workspace_full_analysis(WC=WC, ScanType=args.ScanType, verbose=args.Verbose, StatOnly=StatOnly)
+        print('=================================================\n')
+        #########################
