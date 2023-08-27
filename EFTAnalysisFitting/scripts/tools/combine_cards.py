dda52bf9b0254b45e67fab2f1bd6735d3bea9871
Cole Kampa <ckampa13@gmail.com>
2023-08-26 23:32:34 -0500

    Updated combine_cards.py to work with python2.

--- EFTAnalysisFitting/scripts/tools/combine_cards.py
+++ EFTAnalysisFitting/scripts/tools/combine_cards.py
@@ -9,12 +9,6 @@ from DATACARD_DICT import datacard_dict
 from CONFIG_VERSIONS import versions_dict, WC_ALL
 from MISC_CONFIGS import template_filename, datacard_dir
 
-# check directory exists for signal injection
-# def check_dir(ddir):
-#     if not os.path.isdir(ddir):
-#         print('Creating directory: ', ddir)
-#         os.mkdir(ddir)
-
 # combine subchannels in a channel
 def combine_channel_subchannels(channel, version, datacard_dict, WC, ScanType, StatOnly, SignalInject=False):
     if StatOnly:
@@ -25,25 +19,23 @@ def combine_channel_subchannels(channel, version, datacard_dict, WC, ScanType, S
     sname_ch = datacard_dict[channel]['info']['short_name']
     subchannels = datacard_dict[channel]['subchannels'].keys()
     cmd_str = 'combineCards.py'
-    print(f'Channel: {channel}; Subchannel: ', end='')
+    str_ = 'Channel: %s; Subchannel: ' % channel
     for i, subch in enumerate(subchannels):
-        if i == (len(subchannels)-1):
-            print(subch, end='')
-        else:
-            print(subch,', ', end='')
+        str_ += subch
         sname_sch = datacard_dict[channel]['subchannels'][subch]['info']['short_name']
         # update subchannel name if there is rescaling
         if versions_dict[channel]['lumi'] == '2018':
             sname_sch += '_2018_scaled'
-            print(' (2018 scaled)', end='')
+            str_ += ' (2018 scaled)'
         tfile = template_filename.substitute(channel=sname_ch, subchannel=sname_sch, WC=WC, ScanType=ScanType, purpose='DataCard_Yields', proc=SO_lab, version=version, file_type='txt')
         if SignalInject:
             dc_file = os.path.join(dcdir, channel, version, 'signal_injection_'+WC, tfile)
         else:
             dc_file = os.path.join(dcdir, channel, version, tfile)
-        dc_name = f' sch{sname_sch}'
-        cmd_str += f'{dc_name}={dc_file}'
-    print()
+        dc_name = ' sch%s'% sname_sch
+        cmd_str += '%s=%s' % (dc_name, dc_file)
+        str_ += ', '
+    print(str_)
     # construct output file
     if SignalInject:
         suff_purp = '_SignalInject_'+WC
@@ -51,11 +43,10 @@ def combine_channel_subchannels(channel, version, datacard_dict, WC, ScanType, S
         suff_purp = ''
     tfile_comb = template_filename.substitute(channel=sname_ch, subchannel='_combined', WC=WC, ScanType=ScanType, purpose='DataCard_Yields'+suff_purp, proc=SO_lab, version=version, file_type='txt')
     comb_file = os.path.join(datacard_dir, 'combined_datacards', 'channel', tfile_comb)
-    cmd_str += f'> {comb_file}'
+    cmd_str += '> '+ comb_file
     # run combine script
-    # stdout = subprocess.PIPE
     stdout = None
-    proc = subprocess.run(cmd_str, shell=True, stdout=stdout)
+    proc = subprocess.call(cmd_str, shell=True, stdout=stdout)
 
 # combine all channels
 def combine_all_channels(datacard_dict, WC, ScanType, StatOnly, SignalInject=False):
@@ -66,7 +57,7 @@ def combine_all_channels(datacard_dict, WC, ScanType, StatOnly, SignalInject=Fal
     dcdir = datacard_dir
     channels = datacard_dict.keys()
     cmd_str = 'combineCards.py'
-    print(f'Channel: ', end='')
+    str_ = 'Channel: '
     n_ch_added = 0
     for i, ch in enumerate(channels):
         WCs = versions_dict[ch]['EFT_ops']
@@ -74,15 +65,11 @@ def combine_all_channels(datacard_dict, WC, ScanType, StatOnly, SignalInject=Fal
             continue
         else:
             n_ch_added += 1
-        print(ch, end='')
-        # if i == (len(channels)-1):
-        #     print(ch)
-        # else:
-        #     print(ch,', ', end='')
+        str_ += ch
         v = versions_dict[ch]['v']
-        version = f'v{v}'
+        version = 'v' + str(v)
         if versions_dict[ch]['lumi'] == '2018':
-            print(' (2018 scaled)', end='')
+            str_ + ' (2018 scaled)'
         sname_ch = datacard_dict[ch]['info']['short_name']
         # loop through subchannels
         subchannels = datacard_dict[ch]['subchannels'].keys()
@@ -91,17 +78,15 @@ def combine_all_channels(datacard_dict, WC, ScanType, StatOnly, SignalInject=Fal
             # update subchannel name if there is rescaling
             if versions_dict[ch]['lumi'] == '2018':
                 sname_sch += '_2018_scaled'
-                # print(' (2018 scaled)', end='')
             tfile_ch = template_filename.substitute(channel=sname_ch, subchannel=sname_sch, WC=WC, ScanType=ScanType, purpose='DataCard_Yields', proc=SO_lab, version=version, file_type='txt')
             if SignalInject:
                 dc_file = os.path.join(dcdir, ch, version, 'signal_injection_'+WC, tfile_ch)
             else:
                 dc_file = os.path.join(dcdir, ch, version, tfile_ch)
-            dc_name = f' ch{sname_ch}sch{sname_sch}'
-            cmd_str += f'{dc_name}={dc_file}'
-        if i != (len(channels) - 1):
-            print(', ', end='')
-    print()
+            dc_name = ' ch'+sname_ch+'sch'+sname_sch
+            cmd_str += '%s=%s' % (dc_name, dc_file)
+        str_ += ', '
+    print(str_)
     # stop here if there weren't any WC matches
     if n_ch_added == 0:
         print('No channels have the following WC: '+WC+', combineCards.py will not be run!')
@@ -113,24 +98,23 @@ def combine_all_channels(datacard_dict, WC, ScanType, StatOnly, SignalInject=Fal
         suff_purp = ''
     tfile_comb = template_filename.substitute(channel='all', subchannel='_combined', WC=WC, ScanType=ScanType, purpose='DataCard_Yields'+suff_purp, proc=SO_lab, version='vCONFIG_VERSIONS', file_type='txt')
     comb_file = os.path.join(datacard_dir, 'combined_datacards', 'full_analysis', tfile_comb)
-    cmd_str += f'> {comb_file}'
+    cmd_str += '> ' + comb_file
     # run combine script
-    # stdout = subprocess.PIPE
     stdout = None
-    proc = subprocess.run(cmd_str, shell=True, stdout=stdout)
+    proc = subprocess.call(cmd_str, shell=True, stdout=stdout)
 
 
 if __name__=='__main__':
     # parse commmand line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--Channel',
-                        help=f'Which channel? ["all" (default), "0Lepton_2FJ", "0Lepton_3FJ", "2Lepton_OS", "2Lepton_SS"]')
+                        help='Which channel? ["all" (default), "0Lepton_2FJ", "0Lepton_3FJ", "2Lepton_OS", "2Lepton_SS"]')
     parser.add_argument('-w', '--WC',
-                        help=f'Which Wilson Coefficient to study for 1D limits? ["all" (default), "cW" (default), ...]')
+                        help='Which Wilson Coefficient to study for 1D limits? ["all" (default), "cW" (default), ...]')
     parser.add_argument('-s', '--ScanType',
-                        help=f'What type of EFT scan was included in this file? ["_1D" (default),]')
+                        help='What type of EFT scan was included in this file? ["_1D" (default),]')
     parser.add_argument('-i', '--SignalInject',
-                        help=f'Do you want to use generated signal injection files? If n, default files will be combined. n(default)/y.')
+                        help='Do you want to use generated signal injection files? If n, default files will be combined. n(default)/y.')
     args = parser.parse_args()
     # list of channels
     if args.Channel is None:
@@ -154,7 +138,7 @@ if __name__=='__main__':
     #########################
     # outer loop (over WC)
     for WC in WCs_loop:
-        print(f'WC: '+WC)
+        print('WC: %s' % WC)
         #########################
         # combine channel subchannels
         print('Combining subchannels for each available channel:')
@@ -164,7 +148,7 @@ if __name__=='__main__':
             if not WC in WCs:
                 continue
             v = versions_dict[channel]['v']
-            VERSION = f'v{v}'
+            VERSION = 'v' + str(v)
             for StatOnly in [False, True]:
                 print('Stat only? ', StatOnly)
                 combine_channel_subchannels(channel, VERSION, datacard_dict, WC=WC, ScanType=args.ScanType, StatOnly=StatOnly, SignalInject=SignalInject)
