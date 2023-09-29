54daab019a2b41404ec359cf4b8bdbed478e964b
Cole Kampa <ckampa13@gmail.com>
2023-09-29 11:28:56 -0500

    Plots for previous commit. Added flag for TH1 type in root_file_tools.py

--- EFTAnalysisFitting/scripts/tools/root_file_tools.py
+++ EFTAnalysisFitting/scripts/tools/root_file_tools.py
@@ -1,9 +1,14 @@
 import ROOT
 
-def book_and_set_TH1D(root_out, hist_name, bin_contents, bin_edges, bin_errors=None):
+def book_and_set_TH1D(root_out, hist_name, bin_contents, bin_edges, bin_errors=None, TH1_type='TH1D'):
     # print('hist_name=', hist_name)
     nbins = len(bin_contents)
-    h1d = ROOT.TH1D(hist_name, '', nbins, bin_edges)
+    if TH1_type == 'TH1D':
+        h1d = ROOT.TH1D(hist_name, '', nbins, bin_edges)
+    elif TH1_type == 'TH1F':
+        h1d = ROOT.TH1F(hist_name, '', nbins, bin_edges)
+    else:
+        raise ValueError('TH1_type = "%s" is not allowed. Select between ["TH1D", "TH1F"].' % TH1_type)
     # set bin contents
     for i in range(nbins):
         h1d.SetBinContent(i+1, bin_contents[i])
