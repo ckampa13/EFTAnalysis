d1e3c2d3a36d7c73786bd548f756854269bd02d7
Cole Kampa <ckampa13@gmail.com>
2024-04-26 06:49:49 -0500

    2L_SS v5 finished running. Note that PDF needs to be fixed.

--- EFTAnalysisFitting/scripts/1D_scan/run_Impacts.py
+++ EFTAnalysisFitting/scripts/1D_scan/run_Impacts.py
@@ -28,7 +28,7 @@ from MISC_CONFIGS import (
     dim6_ops,
 )
 
-def get_impact_commands(WC, workspace_file, asimov_str, name_str, json_name,
+def get_impact_commands(WC, workspace_file, asimov_str, name_str, json_name, plot_name,
                         WCs_freeze=None, WCs_limit=None, limit_val=10.):
     params_to_freeze = WCs
     cmd1 = 'combineTool.py -M Impacts -d %s -m 120 %s --redefineSignalPOIs k_%s --freezeNuisanceGroups nosyst ' % (workspace_file, asimov_str, WC)
@@ -57,10 +57,17 @@ def get_impact_commands(WC, workspace_file, asimov_str, name_str, json_name,
         WCs_ = ['k_'+w for w in WCs_limit]
         val = '%0.1f' % limit_val
         mval = '%0.1f' % -limit_val
-        for WC_ in WCs_:
-            cmd1 += ':%s=%s,%s' % (WC_, mval, val)
-            cmd2 += ':%s=%s,%s' % (WC_, mval, val)
-            cmd3 += ':%s=%s,%s' % (WC_, mval, val)
+        cmd1 += ' --setParameterRanges '
+        cmd2 += ' --setParameterRanges '
+        cmd3 += ' --setParameterRanges '
+        for i, WC_ in enumerate(WCs_):
+            if i > 0:
+                cmd1 += ':'
+                cmd2 += ':'
+                cmd3 += ':'
+            cmd1 += '%s=%s,%s' % (WC_, mval, val)
+            cmd2 += '%s=%s,%s' % (WC_, mval, val)
+            cmd3 += '%s=%s,%s' % (WC_, mval, val)
         cmd1 += ' '
         cmd2 += ' '
         cmd3 += ' '
@@ -73,12 +80,19 @@ def get_impact_commands(WC, workspace_file, asimov_str, name_str, json_name,
     cmd2 += '--doFits -n %s' % name_str
     cmd3 += ' -o %s.json -n %s' % (json_name, name_str)
     # plot impacts command
-    cmd4 = 'plotImpacts.py -i %s.json -o $s' % (json_name, json_name)
+    cmd4 = 'plotImpacts.py -i %s.json -o %s' % (json_name, plot_name)
     cmds = [cmd1, cmd2, cmd3, cmd4]
     return cmds
 
 def run_impact_subchannels(dim, channel, version, datacard_dict, WC, ScanType, Asimov, asi_str,
-                           SignalInject, Precision, PrecisionCoarse, stdout, verbose=0):
+                           SignalInject, stdout, verbose=0):
+    start_dir = os.getcwd()
+    syst_label='syst'
+    if ScanType == '_1D':
+        scan_dir = 'freeze'
+    else:
+        scan_dir = 'profile'
+    plot_dir = os.path.join(datacard_dir, 'plots', 'subchannel', scan_dir)
     if ScanType == '_1D':
         ScanTypeWS = '_All'
     else:
@@ -113,7 +127,33 @@ def run_impact_subchannels(dim, channel, version, datacard_dict, WC, ScanType, A
                     WCs_limit.append(WC_)
                 elif (dim == 'dim8') and (not WC_ in dim6_ops):
                     WCs_limit.append(WC_)
-    wsfile = template_filename.substitute(channel=sname_ch, subchannel=sname_sch, WC=dim, ScanType=ScanTypeWS, purpose='workspace'+suff_purp, proc=SO_lab, version=version, file_type='root')
+    sname_ch = datacard_dict[channel]['info']['short_name']
+    subchannels = datacard_dict[channel]['subchannels'].keys()
+    print('Channel: %s' % channel)
+    for i, subch in enumerate(subchannels):
+        print('Subchannel: %s' % subch)
+        sname_sch = datacard_dict[channel]['subchannels'][subch]['info']['short_name']
+        # update subchannel name if there is rescaling
+        if versions_dict[channel]['lumi'] == '2018':
+            sname_sch += '_2018_scaled'
+        # construct workspace filename
+        SO_lab = ''
+        wsfile = template_filename.substitute(channel=sname_ch, subchannel=sname_sch, WC=dim, ScanType=ScanTypeWS, purpose='workspace'+suff_purp, proc=SO_lab, version=version, file_type='root')
+        wsfile = os.path.join(wsdir, wsfile)
+        name_str = 'Impacts'+template_outfilename_stub.substitute(asimov=asi+suff_purp, channel=sname_ch,subchannel=sname_sch,WC=WC,ScanType=ScanType,version=version,syst=syst_label)
+        json_name = 'Impacts'+template_outfilename_stub.substitute(asimov=asi+suff_purp, channel=sname_ch,subchannel=sname_sch,WC=WC,ScanType=ScanType,version=version,syst=syst_label)
+        plot_name = os.path.join(plot_dir, 'Impacts'+template_outfilename_stub.substitute(asimov=asi+suff_purp, channel=sname_ch,subchannel=sname_sch,WC=WC,ScanType=ScanType,version=version,syst=syst_label))
+        # print("json_name: %s, type: %s" % (json_name, str(type(json_name))))
+        # print("plot_name: %s, type: %s" % (plot_name, str(type(plot_name))))
+        cmds = get_impact_commands(WC, wsfile, asi_str, name_str, json_name, plot_name,
+                                   WCs_freeze=WCs_freeze, WCs_limit=[WC], limit_val=10.)
+        for cmd in cmds:
+            print(cmd)
+            proc = subprocess.call(cmd, stdout=stdout, shell=True)
+            print('\n\n')
+    # go back to original directory
+    print('Going back to original directory...')
+    os.chdir(start_dir)
 
 
 if __name__=='__main__':
@@ -153,6 +193,14 @@ if __name__=='__main__':
             raise ValueError('Unrecognrized analysis level "%s". Please rerun with one of the following: ["s", "c", "f"]')
     if args.ScanType is None:
         args.ScanType = '_All'
+    if args.Asimov is None:
+        args.Asimov = 'y'
+    if args.Asimov == 'y':
+        asi_str = '-t -1'
+        args.Asimov=True
+    else:
+        asi_str = ''
+        args.Asimov=False
     if args.SignalInject is None:
         SignalInject = False
     else:
@@ -165,6 +213,7 @@ if __name__=='__main__':
         stdout = None
     else:
         stdout = subprocess.PIPE
+    WC = args.WC
     print('WC: %s' % WC)
     if WC in dim6_ops:
         dim = 'dim6'
@@ -172,19 +221,16 @@ if __name__=='__main__':
         dim = 'dim8'
     print(dim)
     # run the appropriate function
+    channel = args.Channel
     if generate_subch:
         print('Making impact plot for each subchannel:')
         print('=================================================')
         WCs = versions_dict[channel]['EFT_ops']
-        if not WC in WCs:
-            continue
-        v = versions_dict[channel]['v']
-        VERSION = 'v'+str(v)
-        run_impact_subchannels(dim, channel, VERSION, datacard_dict, WC=WC,
-                         ScanType=args.ScanType, Asimov=args.Asimov, asi_str=asi_str,
-                         SignalInject=SignalInject, Precision=args.Precision,
-                         PrecisionCoarse=args.PrecisionCoarse,
-                         stdout=stdout, verbose=args.Verbose)
+        if WC in WCs:
+            v = versions_dict[channel]['v']
+            VERSION = 'v'+str(v)
+            run_impact_subchannels(dim, channel, VERSION, datacard_dict, WC=WC,
+                                   ScanType=args.ScanType, Asimov=args.Asimov,
+                                   asi_str=asi_str, SignalInject=SignalInject,
+                                   stdout=stdout, verbose=args.Verbose)
     print('=================================================\n')
-
-
