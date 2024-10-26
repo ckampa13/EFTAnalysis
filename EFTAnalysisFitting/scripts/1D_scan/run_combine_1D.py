51ef1eac9514855d0e1d60cbed45cb19c5cfaadf
Cole Kampa <ckampa13@gmail.com>
2024-10-26 16:10:52 -0500

    cHq3, cHq1, cHu, cHd, cHW, cHWB, cHB, cHbox, cHDD, cll1 varying. cW and cHl3 run.

--- EFTAnalysisFitting/scripts/1D_scan/run_combine_1D.py
+++ EFTAnalysisFitting/scripts/1D_scan/run_combine_1D.py
@@ -365,8 +365,8 @@ def run_combine_channels(dim, channels, datacard_dict, WC, ScanType, Asimov, asi
         # adding others with <1% total contribution (quad)
         # if not WC == 'cHDD':
         #     WCs_freeze.append('cHDD')
-        if not WC == 'cll1':
-            WCs_freeze.append('cll1')
+        # if not WC == 'cll1':
+        #     WCs_freeze.append('cll1')
         # if not WC == 'cHWB':
         #     WCs_freeze.append('cHWB')
         # if not WC == 'cHbox':
@@ -378,8 +378,8 @@ def run_combine_channels(dim, channels, datacard_dict, WC, ScanType, Asimov, asi
         #     WCs_freeze.append('cHq1')
         # if not WC == 'cHW':
         #     WCs_freeze.append('cHW')
-        # if not WC == 'cHl3':
-        #     WCs_freeze.append('cHl3')
+        if not WC == 'cHl3':
+            WCs_freeze.append('cHl3')
         #WCs_limit = None
         #'''
         # BETTER
