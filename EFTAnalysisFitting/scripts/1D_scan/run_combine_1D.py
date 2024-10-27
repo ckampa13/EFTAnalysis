1b55c1e214694fc7f1910fcb3febeb70068e5575
Cole Kampa <ckampa13@gmail.com>
2024-10-27 13:14:11 -0500

    cHDD, cll1, cHbox frozen. limit +-200.

--- EFTAnalysisFitting/scripts/1D_scan/run_combine_1D.py
+++ EFTAnalysisFitting/scripts/1D_scan/run_combine_1D.py
@@ -378,8 +378,8 @@ def run_combine_channels(dim, channels, datacard_dict, WC, ScanType, Asimov, asi
             WCs_freeze.append('cll1')
         # if not WC == 'cHWB':
         #     WCs_freeze.append('cHWB')
-        # if not WC == 'cHbox':
-        #     WCs_freeze.append('cHbox')
+        if not WC == 'cHbox':
+            WCs_freeze.append('cHbox')
         # adding all the others...
         # if not WC == 'cHq3':
         #     WCs_freeze.append('cHq3')
