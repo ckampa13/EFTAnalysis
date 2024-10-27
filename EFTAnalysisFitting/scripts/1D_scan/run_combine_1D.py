806308b785f6d2ed2fe34cff67ba5543d7aff32a
Cole Kampa <ckampa13@gmail.com>
2024-10-27 10:13:54 -0500

    Same as previous, all WCs in --trackParameters.

--- EFTAnalysisFitting/scripts/1D_scan/run_combine_1D.py
+++ EFTAnalysisFitting/scripts/1D_scan/run_combine_1D.py
@@ -45,7 +45,7 @@ LIM_VAL = 50
 # extra options
 secret_options = """ --robustFit=1 --setRobustFitTolerance=0.2 --cminDefaultMinimizerStrategy=0 \
 --X-rtd=MINIMIZER_analytic --X-rtd MINIMIZER_MaxCalls=99999999999 --cminFallbackAlgo Minuit2,Migrad,0:0.2 \
---stepSize=0.005 --X-rtd FITTER_NEW_CROSSING_ALGO --X-rtd FITTER_NEVER_GIVE_UP --X-rtd FITTER_BOUND --saveSpecifiedNuis all --trackParameters k_cHq3
+--stepSize=0.005 --X-rtd FITTER_NEW_CROSSING_ALGO --X-rtd FITTER_NEVER_GIVE_UP --X-rtd FITTER_BOUND --saveSpecifiedNuis all --trackParameters k_cW,k_cHq3,k_cHq1,k_cHu,k_cHd,k_cHW,k_cHWB,k_cHl3,k_cHB,k_cll1,k_cHbox,k_cHDD
 """
 
 # for finding appropriate scan range
