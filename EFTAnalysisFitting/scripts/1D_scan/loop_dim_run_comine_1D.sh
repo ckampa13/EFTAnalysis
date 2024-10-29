7994ea1d8afe0984e1a91741186d4648bb27c161
Cole Kampa <ckampa13@gmail.com>
2024-10-29 00:00:58 -0500

    Rerun of full analysis for all dim6, all dim8.

--- EFTAnalysisFitting/scripts/1D_scan/loop_dim_run_comine_1D.sh
+++ EFTAnalysisFitting/scripts/1D_scan/loop_dim_run_comine_1D.sh
@@ -5,6 +5,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}"  )" &> /dev/null && pwd  
 LOGDIR=$SCRIPT_DIR'/../../output/logs/'
 echo "LOGDIR=${LOGDIR}"
 
+WCs_dim6=("cW" "cHbox" "cHDD" "cHl3" "cHq1" "cHq3" "cHW" "cHWB" "cll1" "cHB" "cHu" "cHd")
+WCs_dim8=("FS0" "FS1" "FS2" "FM0" "FM1" "FM2" "FM3" "FM4" "FM5" "FM7" "FT0" "FT1" "FT2" "FT3" "FT4" "FT5" "FT6" "FT7" "FT8" "FT9")
+
 if [[ -z "$1" ]]; then
     L="f"
 else
@@ -23,8 +26,20 @@ else
     DIM=$3
 fi
 
-for i in $(seq 0 12);
+# pick appropriate WC
+if [[ "$DIM" == "dim6" ]]; then
+    selected_list=("${WCs_dim6[@]}")
+    P="0.005"
+elif [[ "$DIM" == "dim8" ]]; then
+    selected_list=("${WCs_dim8[@]}")
+    P="0.05"
+else
+    echo "Error: DIM must be either 'dim6' or 'dim8'."
+        exit 1
+fi
+
+for WC in "${selected_list[@]}";
 do
-    echo Running combine for clip index $i, sent to background...
-    python 1D_scan/run_combine_1D.py -c $CH -w $DIM -t $L -s _1D -a y -p 0.05 -pc 0.5 -v _clip_mVVV_$i > ${LOGDIR}${CH}_${DIM}_${L}_clip_mVVV_${i}.txt 2>&1 &
+    echo Running combine for WC $WC, sent to background...
+    python 1D_scan/run_combine_1D.py -c $CH -w $WC -t $L -s _1D -a y -p ${P} -pc 0.5 > ${LOGDIR}main_ana_${CH}_${DIM}_${WC}_${L}.txt 2>&1 &
 done
