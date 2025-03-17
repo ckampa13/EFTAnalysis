369d520f420750fc3193e84f68594a4a10830dbb
Saptaparna <saptaparna@gmail.com>
2025-03-17 10:45:43 -0700

    Update

--- NDim_2017/submitCondorJob.sh
+++ NDim_2017/submitCondorJob.sh
@@ -18,21 +18,21 @@ LogDir=CondorJobs
 [ -d $LogDir ] || mkdir ${LogDir};
 
 
-Nevents=500
+Nevents=1000
    
 # if run on the cmsplc, can not use remap, use -f options here to copy the file
-cat>Job_${1}.sh<<EOF
+cat>Job_${SAMPLE}-${LEPTONFILTER}-${CAMPAIGN}_${1}.sh<<EOF
 #!/bin/bash
 sh triboson_production.sh -p pileup_files.txt -s ${SAMPLE} -c -o $PWD -a ${1} -n ${Nevents} -b \${2}\${3} -l ${LEPTONFILTER} -f
 EOF
 
-chmod 775 Job_${1}.sh
+chmod 775 Job_${SAMPLE}-${LEPTONFILTER}-${CAMPAIGN}_${1}.sh
 
 # x509up_u100637, ${X509_USER_PROXY} doesn't work on cms connect 
 
-cat>condor_${1}.jdl<<EOF
+cat>condor_${SAMPLE}-${LEPTONFILTER}-${CAMPAIGN}_${1}.jdl<<EOF
 universe = vanilla
-Executable = Job_${1}.sh
+Executable = Job_${SAMPLE}-${LEPTONFILTER}-${CAMPAIGN}_${1}.sh
 Arguments = ${1} \$(Cluster) \$(Process)
 Requirements = OpSys == "LINUX" && (Arch != "DUMMY" )
 request_disk = 10000000
@@ -41,9 +41,9 @@ Should_Transfer_Files = YES
 WhenToTransferOutput = ON_EXIT
 transfer_input_files = Job_${1}.sh, triboson_production.sh, pileup_files.txt, CMSSW_10_6_26.tar.gz
 notification = Never
-Output = ${LogDir}/STDOUT_\$(Cluster)\$(Process)${1}.stdout
-Error = ${LogDir}/STDERR_\$(Cluster)\$(Process)${1}.stderr
-Log = ${LogDir}/LOG_\$(Cluster)\$(Process)${1}.log
+Output = ${LogDir}/STDOUT_\$(Cluster)\$(Process)${1}_${SAMPLE}-${LEPTONFILTER}-${CAMPAIGN}.stdout
+Error = ${LogDir}/STDERR_\$(Cluster)\$(Process)${1}_${SAMPLE}-${LEPTONFILTER}-${CAMPAIGN}.stderr
+Log = ${LogDir}/LOG_\$(Cluster)\$(Process)${1}_${SAMPLE}-${LEPTONFILTER}-${CAMPAIGN}.log
 x509userproxy = ${X509_USER_PROXY}
 +ApptainerImage = "/cvmfs/singularity.opensciencegrid.org/cmssw/cms:rhel7" 
 +MaxRuntime           = 86400
@@ -51,7 +51,7 @@ Queue 1
 EOF
 
 if [ "${2}" = "run" ]; then
-   condor_submit condor_${1}.jdl
+   condor_submit condor_${SAMPLE}-${LEPTONFILTER}-${CAMPAIGN}_${1}.jdl
    echo "submit condor job"
 else
    echo 'dry run'
